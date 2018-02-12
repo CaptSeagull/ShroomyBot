13884be9b90589f9bdf242bb19e74a5877884411
CaptSeagull <30291691+CaptSeagull@users.noreply.github.com>
2018-02-12 14:29:48 -0800

    Added:  - getRandomQuote() to retrieve a random quote from the Forismatic API. Modified:  - getPokemon to stache results for less calls.  - all API calls now return a dict result

--- commons.py
+++ commons.py
@@ -1,12 +1,87 @@
 from random import randint
-import re
+import os
+import json
 
-def getRandomInt(size):
+def getRandomInt(size:int):
     return randint(0, size - 1)
 
 def getRandomTuple(items):
-    pos = getRandomInt( len( items ) )
-    return items[pos]
+    if items:
+        pos = getRandomInt( len( items ) )
+        return items[pos]
+    return items
 
-def getSplit(text, delimiter_pattern=(',|\\b(or)')):
-    return re.split(delimiter_pattern, text)
+def getJsonFromFile(directory:str="/",filename:str="",relative_dir:bool=False,filepath:str=None):
+    result = {'success':True,'json':{},'filepath':""}
+
+    abs_filename_path = ""
+
+    # If filepath is not set, retrieve from directory and filename
+    if not filepath:
+        if relative_dir is True:
+            relative_path = os.path.dirname(__file__)
+            directory = os.path.join(relative_path, directory)
+        # Directory location, we need to create directory if not exists
+        if not os.path.exists(directory):
+            try:
+                os.makedirs(directory)
+            except OSError as e:
+                print("OSError: " + str(e))
+                result['success'] = False
+                return result
+        # File's path, create a json file if not exists; otherwise, we parse the json file data
+        abs_filename_path = os.path.join(directory, filename)
+    else:
+        abs_filename_path = filepath
+
+    result['filepath'] = abs_filename_path
+    if os.path.exists(abs_filename_path):
+        # If file already exists, try to read the contents
+        try:
+            with open(abs_filename_path, "r") as f:
+                result['json'] = json.load(f)
+        except ValueError as e:
+            print("ValueError: " + str(e))
+    else:
+        # If no file exists, try to create the file by dumping an empty json file
+        try:
+            with open(abs_filename_path, "w") as f:
+                json.dump(result['json'], f)
+        except Exception as e:
+            print("Writing exception: " + str(e))
+            result['success'] = False
+
+    return result
+
+def updateJsonFileContents(result_dict:dict={},key_to_update:str=None,filepath:str=""):
+    if not result_dict:
+        return False
+
+    dict_to_save = {}
+    
+    # Create a copy so the original is intact
+    result_copy = dict(result_dict)
+    # Remove error key before saving
+    result_copy.pop('error', None)
+
+    # First if key_to_update exists, we need to retrieve the updated file (if file doesn't exist, create)
+    if key_to_update:
+        result_updated = getJsonFromFile(filepath=filepath)
+        success = result_updated.get('success', False)
+        dict_to_save = result_updated.get('json', {})
+        if success is True:
+            dict_to_save[key_to_update] = result_copy
+        else:
+            return False
+    # Otherwise we assume result_dict is the entire file
+    else:
+        dict_to_save = result_copy
+        
+    # Now to save
+    try:
+        with open(filepath, 'w') as f:
+            json.dump(dict_to_save, f)
+    except Exception as e:
+        print("Exception when writing the file: " + str(e))
+        return False
+    return True
--- otherapi.py
+++ otherapi.py
@@ -3,22 +3,119 @@ import json
 from xml.etree import ElementTree
 import urllib
 
-def getPokemon(name):
-    url = 'http://pokeapi.co/api/v2/'
-    command = 'pokemon/'
+# Personal Library
+import commons
 
-    if not name:
-        return "No pokemon entered."
-    r = requests.post(url + command + name.lower() + "/")
+def getPokemon(query=""):
+    '''Retrive requested pokemon info from json file. If none exists, retrive from getPokemonFromApi(...) '''
+    # This is where we will store json results (from file or API)
+    pkmn_json = {}
+
+    # Retrieve relative path to the json file
+    rel_path = "pkmn/"
+    filename = "pkmn_names.json"
+
+    # Read from the json file, create if needed
+    json_file_result = commons.getJsonFromFile(rel_path,filename,True)
+    has_file = json_file_result.get('success',False)
+    filepath = json_file_result.get('filepath',"/")
+
+    # If json file was read, retrieve contents to see if pkmn entry exists
+    pkmn_json = json_file_result.get('json',{})
+    pkmn_result = pkmn_json.get(query, "")
+    if not pkmn_result:
+        pkmn_result = getPokemonFromApi(query)
+        # If there were issues earlier reading/creating the json file simply return the result
+        # Otherwise, try to save the file if there was no error
+        if not pkmn_result['error'] and has_file is True:
+            commons.updateJsonFileContents(pkmn_result,query,filepath)
+    return pkmn_result
+
+def getPokemonFromApi(query=""):
+    '''Retrieve pokemon info from PokeAPI '''
+    site_url = "https://pokeapi.co/"
+    url = 'http://pokeapi.co/api'
+    version = 'v2'
+    command = 'pokemon'
+
+    # How many digits are needed for pkmn id's
+    pkmn_digits = 3
+    
+    # Default MissingNo values
+    default_id = 0
+    default_name = "MissingNo"
+    default_sprite = "" # To Do?
+    default_types = ["None"]
+
+    # Wrapper to return result
+    pkmn_wrapper = {
+        'pkmn_id':str( default_id ).zfill(pkmn_digits),
+        'pkmn_name':default_name,
+        'pkmn_sprite':default_sprite,
+        'pkmn_types':default_types,
+        'source':site_url,
+        'error':""
+        }
+
+    # If query is not empty
+    if query:
+        r = requests.post("{0}/{1}/{2}/{3}/".format(url, version, command, str(query).lower()))
+        # Debug. Uncomment if needed
+        # print(r.url)
+
+        # Success
+        if r.status_code == 200:
+            result_dict = r.json()
+
+            # Retrieve needed values
+            # Default to MissingNo values if needed
+            pkmn_id = result_dict.get('id', default_id)
+            pkmn_name = result_dict.get('name', default_name)
+            pkmn_sprite = result_dict.get('sprites', dict(front_default="")).get('front_default',default_sprite)
+            
+            pkmn_types = []
+            pkmn_types_list = result_dict.get('types', [])
+            for pkmn_types_list_dict in pkmn_types_list:
+                pkmn_types.append( pkmn_types_list_dict.get('type',{}).get('name',"none") )
+
+            # Add results to dict
+            pkmn_wrapper.update({
+                'pkmn_id':str( pkmn_id, ).zfill(pkmn_digits),
+                'pkmn_name':pkmn_name,
+                'pkmn_sprite':pkmn_sprite,
+                'pkmn_types':pkmn_types,
+                })
+            
+        elif r.status_code == 404:
+            pkmn_wrapper['error'] = "No pokemon found for {0}".format(str(query))
+        else:
+            pkmn_wrapper['error'] = "Something bad happened. Error Code: " + str(r.status_code)
+    else:
+        pkmn_wrapper['error'] = "No valid pkmn name entered. (e.g. Pikachu)"
+        
+    return pkmn_wrapper
+
+def getRandomQuote():
+    site_url = "https://forismatic.com/en/"
+    url = 'https://api.forismatic.com/api'
+    version = '1.0/'
+    params = {'method':"getQuote",'format':"json",'lang':"en"}
+
+    result_dict = dict(error="")
+    r = requests.post("{0}/{1}/".format(url, version), params=params)
     if r.status_code == 200:
         result = r.json()
-        return "Pokedex#{0} - {1}\n{2}".format(str(result["id"]), result["name"], result["sprites"]["front_default"])
-    elif r.status_code == 404:
-        return "Could not find the pokemon requested."
-    else:
-        return "Something else happened. Error Code: " + str(r.status_code)
+        result_dict = {
+            'quote':result.get('quoteText',""),
+            'author':result.get('quoteAuthor',""),
+            'url':result.get('quoteLink',site_url),
+            'error':""
+            }
+        return result_dict
+    return dict(error="No quote found.")
 
-def getSpanishMiriamWebster(word, key_id = ""):
+'''
+def getSpanishMiriamWebster(word, key_id=""):
     url = 'https://www.dictionaryapi.com/api/v1/references/spanish/xml/'
 
     if not word or not key_id:
@@ -33,10 +130,9 @@ def getSpanishMiriamWebster(word, key_id = ""):
             return tree_branch.text   
                       
         return "Could not find the translated word."
-    elif r.status_code == 404:
-        return "Could not find the translated word."
     else:
         return "Something else happened. Error Code: " + str(r.status_code)
+'''
 
 def getJishoPage(query):
     # Dict for our results
@@ -44,45 +140,54 @@ def getJishoPage(query):
         'writing':"",
         'reading':"",
         'definitions':[],
-        'speech_type':"",
+        'speech_type':[],
         'error':""
         }
 
     # GET command
-    url = 'http://jisho.org/api/v1/search/words'
-    params = urllib.parse.urlencode(dict(keyword=query), quote_via=urllib.parse.quote)
-    r = requests.get(url, params=params)
+    url = 'http://jisho.org/api'
+    version = 'v1'
+    command = 'search/words'
+    
+    params = urllib.parse.urlencode(dict(keyword=query))
+    r = requests.get("{0}/{1}/{2}".format(url, version, command), params=params)
     if r.status_code == 200:
         return parseJishoPage(r.json(), wrapper)
-    elif r.status_code == 404:
-        wrapper['error'] = "Could not find the word requested. Error Code: " + str(r.status_code)
     else:
         wrapper['error'] =  "Something else happened. Error Code: " + str(r.status_code)
     return wrapper
 
-def parseJishoPage(json, wrapper, result_index = 0):
-    if json['data']:
-        if result_index > len( json['data'] ) - 1:
-            result_index = len( json['data'] ) - 1
+def parseJishoPage(json, wrapper, result_index=0):
+    data_list = json.get('data', [])
+    data_size = len( data_list )
+    
+    if data_list:
+        if result_index > data_size - 1:
+            result_index = data_size - 1
         # Retrieve the result based on the result_index
-        data_list = json['data'][result_index]
-        # Retrieve writing and reading
-        if data_list['japanese']:
-            jp_list = data_list['japanese'][0]
-            if 'word' in jp_list:
-                wrapper['writing'] = jp_list['word']
-            if 'reading' in jp_list:
-                wrapper['reading'] = jp_list['reading']
-        if data_list['senses']:
-            senses_list = data_list['senses'][0]
+        data_dict = data_list[result_index]
+        jp_list = data_dict.get('japanese', [])
+        senses_list = data_dict.get('senses', [])
+        
+        if jp_list:
+            jp_dict = jp_list[0] # Retrieve first item (should be only item)
+
+            # Retrieve writing and reading
+            wrapper['writing'] = jp_dict.get('word', "")
+            wrapper['reading'] = jp_dict.get('reading', "")
+            
+        if senses_list:
+            senses_dict = senses_list[0] # Retrieve first item (should be only item)
+            eng_def_list = senses_dict.get('english_definitions', [])
+            speech_type_list = senses_dict.get('parts_of_speech', [])
+
             # Retrieve english definitions
-            if senses_list['english_definitions']:
-                for eng_def in senses_list['english_definitions']:
-                    wrapper['definitions'].append(eng_def)
+            for eng_def in eng_def_list:
+                wrapper['definitions'].append(eng_def)
             # Retrieve speech type
-            if senses_list['parts_of_speech']:
-                wrapper['speech_type'] = senses_list['parts_of_speech'][0]
+            for speech in speech_type_list:
+                wrapper['speech_type'].append(speech)
         return wrapper
 
-    wrapper['error'] = "Could not find the word requested."
+    wrapper['error'] = "No results found for the word requested."
     return wrapper
